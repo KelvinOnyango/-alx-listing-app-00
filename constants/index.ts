e513fcd9e3bdf8d4e7b158d15de5750cfe692ff5
Kelvin Onyango <kelvinonyango155@gmail.com>
2025-05-31 22:48:25 -0700

    Update project files

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,9 @@
+import { PropertyProps } from "@/interfaces";
+
+export const PROPERTYLISTINGSAMPLE: PropertyProps[] = [
+  // Include all the property data exactly as provided in the instructions
+  // (The full array from the instructions goes here)
+];
 export const Description: { title: string; description: string } = {
   title: "Constants",
   description:
